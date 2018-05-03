847a85f7e4366dff695c677f7be7559fedcdd29e
wenhao-yap <yap.justin111@gmail.com>
2018-05-03 18:54:01 +0800

    added image and anchor link

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ class FetchTweets extends React.Component {
   render() {
     const tweet = this.props.data.map (
       details => {
+        const regex = /((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)/g;
+        const link = details.text.match(regex);
+        const text = details.text.replace(regex,"");
+        let image = "";
+        if (details.extended_entities){
+          image = details.extended_entities.media['0'].media_url_https
+        }
 
         return <div key={details.id}>
           <h5>{details.created_at}</h5>
-          <p>{details.text}</p>
+          <p>{text} <a href={link} target="_blank">{link}</a></p>
+          <img src={image}/>
           <ul>
             <li>Retweet Count: {details.retweet_count} </li>
             <li>Favourite Count: {details.favorite_count}</li> 

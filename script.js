4e1252d9feba8e9dc9437426ce0ab3bae5e18fe0
wenhao-yap <yap.justin111@gmail.com>
2018-05-03 18:17:51 +0800

    minor change

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ class FetchTweets extends React.Component {
     const tweet = this.props.data.map (
       details => {
 
-        let text = details.text.replace(
-        /((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)/g,
-        "");
-
         return <div key={details.id}>
           <h5>{details.created_at}</h5>
           <p>{details.text}</p>
-          
           <ul>
             <li>Retweet Count: {details.retweet_count} </li>
             <li>Favourite Count: {details.favorite_count}</li> 
